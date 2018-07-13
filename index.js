2ec2209a888c9b9a44240bcdf2fbb207f31c78c5
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-07-13 19:08:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function status1() {
 
  robot.on('guildMemberAdd', (member) => {
       member.addRole('459206853822251018')
-                                                        
+                                                     
  });
 
 
@@ -34,13 +34,24 @@ message.channel.send('Pinging...').then(sent => {
 });
 
 robot.on('message', message => {
-                                                            if(message.content.startsWith(p + 'say')) {
-                                                                message.delete()
-                                                                let say = message.content.slice((p + 'say').length);
-                                                                message.channel.send(say);
-                                                                console.log("ко-ко повторюшка")
-                                                            }
-                                                        });
+            if(message.content.startsWith(p + 'say')) {
+                   message.delete()
+                   let say = message.content.slice((p + 'say').length);
+                      message.channel.send(say);
+                     console.log("ко-ко повторюшка")
+	    }
+	    });
+
+
+robot.on("guildCreate", guild => {
+	console.log(`Меня добавили на сервер: ${guild.name} (id: ${guild.id}). На этом сервере ${guild.memberCount} участников!`);
+});
+
+robot.on("guildDelete", guild => {
+	console.log(`Меня выгнали из: ${guild.name} (id: ${guild.id})`);
+});
+
+
 
 
 robot.login(process.env.BOT_TOKEN);
