4ab015db0d55a93ed73329f0fbd776eb5e423713
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-07-16 11:33:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fs =require("fs")
 var p = "!"
 
 robot.on('ready', () => {
-    robot.user.setActivity('Вход в Дискорд аккаунт',{ type: "PLAYING" })
+    robot.user.setActivity('Loading.. Мяу',{ type: "PLAYING" })
     robot.user.setStatus('dnd')
-    setTimeout(status1, 20000)
+    setTimeout(status1, 12000)
     console.log('login....')
 });
 
@@ -18,11 +18,10 @@ function status1() {
 
 
 
- robot.on('guildMemberAdd', (member) => {
-      member.addRole('459206853822251018')
-                                                     
- });
-
+robot.on('guildMemberAdd', (member) => {
+	member.addRole('468063445590343681')
+	
+});
 
 robot.on('message', message => {
   if (message.content === (p + 'ping')) {
@@ -36,6 +35,7 @@ message.channel.send('Pinging...').then(sent => {
 robot.on('message', message => {
             if(message.content.startsWith(p + 'say')) {
                    message.delete()
+		    if(!message.member.roles.some(r=>["Модератор", "Стример"].includes(r.name)) )
 		    if(message.author.id !== '405258156063850497')
 return message.reply("Прости, но ты не можешь использовать это!")
                    let say = message.content.slice((p + 'say').length);
@@ -53,6 +53,47 @@ robot.on("guildDelete", guild => {
 	console.log(`Меня выгнали из: ${guild.name} (id: ${guild.id})`);
 });
 
+
+
+robot.on('message', message => {
+	if(message.content.startsWith(p + 'nick')) {
+var mentions1 = message.mentions
+const vtes = message.content.slice(28);
+if(message.author.id !== '405258156063850497')
+			     return message.reply("Прости, но ты не можешь использовать это!")
+message.delete()
+message.mentions.members.first().setNickname(vtes)
+console.log(vtes)
+
+
+		robot.on('message', message => {
+    if(message.content.startsWith(p + 'delete')) {
+                   if(!message.member.roles.some(r=>["Модератор", "Стример"].includes(r.name)) )
+		   if(message.author.id !== '292178755760422915')
+      return message.reply("Прости, но ты не можешь использовать это!")
+        message.delete()
+        let delmes = message.content.slice((p + 'delete').length);
+        var result = 'Успешно удалено' + delmes + ' сообщений'
+        message.channel.bulkDelete(delmes)
+        message.channel.send(result).then((res) => {
+        setTimeout(()=>{res.delete()},5000)
+        console.log('Кто-то удалил сообщения!')
+        })
+    }
+});
+		
+	
+		robot.on('message', message => {
+	if(message.content.startsWith(p + 'avatar')) {
+		const embed = new Discord.RichEmbed()
+		.setTitle('Аватар пользователя:')
+            .setColor('RANDOM')
+		 .setImage(message.mentions.users.first().avatarURL)
+		 message.channel.send({embed})
+		console.log('Кому то понадобился аватар')
+	}
+});
+		
 
 
 
