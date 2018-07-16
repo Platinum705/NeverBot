a3e5da0c167ed12425066184a03f5c646af832b6
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-07-16 12:06:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function status1() {
 
 robot.on('guildMemberAdd', (member) => {
 	member.addRole('468063445590343681')
-	var koko = "Меня добавили на сервер: ${guild.name} (id: ${guild.id}). На этом сервере ${guild.memberCount} участников!"
+	var koko = "Новый холоп прибыл в: ${guild.name} (id: ${guild.id}). На этом сервере ${guild.memberCount} участников!"
 	console.log(koko)
 });
 
