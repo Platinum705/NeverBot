12c1cd4d91b618f1e125e420bdf651c5696d8412
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-07-27 12:18:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,11 @@ robot.on('ready', () => {
 
 
 function status1() {
-    robot.user.setActivity('Аниме',{ type: "WATCHING" })
+    robot.user.setActivity('Re:Zero',{ type: "WATCHING" })
     robot.user.setStatus('online')
 	setTimeout(status2, 300000)
 }
 
-function status2() {
-    robot.user.setActivity('Re:Zero',{ type: "WATCHING" })
-    robot.user.setStatus('online')
-	
-	
-}
 
 
 
