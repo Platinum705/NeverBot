1e6134166c044c32160ee8518fbfb0f157bc1008
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-12-28 07:35:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -176,13 +176,12 @@ if(message.content == "!ивент"){
 }});
 */
 robot.on('message', message => {
-  if (message.content === (p + 'event')) {
+  if (message.content === (p + 'start')) {
 	  setInterval(function() {
 message.channel.send("@everyone, доброго времени суток, до мирового босса осталось 10 минут");
 	
-		
+		}, 10000)
 	  
-    }, 86400000)
 		    
   }
 });
